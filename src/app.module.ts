173d85a2e27ab0d59741abd43656e4b528daa3fa
lucasmassing <202782@upf.br>
2025-11-16 13:47:25 -0300

    criado modulo de profile e testes unitários

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { ProfilesModule } from './profiles/profiles.module';
 @Module({
   imports: [
     //adicionado aqui global o configModule
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     UsersModule,
+    ProfilesModule,
   ],
 })
 export class AppModule { }
